cb90cd2415ef5ea74c3ab0feed078cf217d0588b
Vinnick <romanvinnick@gmail.com>
2022-03-01 23:20:01 +0100

    added card

---
+++ src/components/Card.tsx
@@ -0,0 +1,10 @@
+interface CardProps {
+  width: string
+  height: string
+}
+
+const Card = ({ width, height }: CardProps) => {
+  return <div style={{ width, background: 'grey' }}>asdfasd</div>
+}
+
+export default Card
