9cc90726a454521c1a5fa98b2ca776a79e8945af
Vinnick <romanvinnick@gmail.com>
2022-03-06 14:56:31 +0100

    restyled the card

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,12 @@ import './App.css'
 import Card, { CardVariant } from './components/Card/Card'
 
 function App() {
-  const arr = [
-    { height: '30px', width: '30px', variant: CardVariant.primary },
-    { height: '30px', width: '30px', variant: CardVariant.primary },
-    { height: '30px', width: '30px', variant: CardVariant.primary },
-    { height: '30px', width: '30px', variant: CardVariant.outlined },
-    { height: '30px', width: '30px', variant: CardVariant.outlined },
-    { height: '30px', width: '30px', variant: CardVariant.outlined },
-    { height: '30px', width: '30px', variant: CardVariant.outlined },
-    { height: '30px', width: '30px', variant: CardVariant.outlined },
-    { height: '30px', width: '30px', variant: CardVariant.outlined },
-  ]
-
   return (
     <div className="App">
-      {arr.map((e, idx) => {
-        return (
-          <Card
-            key={idx}
-            width={e.width}
-            height={e.height}
-            variant={e.variant}
-          ></Card>
-        )
-      })}
+      <Card width="300px" height="300px" variant={CardVariant.outlined}></Card>
+      <Card width="300px" height="300px" variant={CardVariant.primary}>
+        <button>press</button>
+      </Card>
     </div>
   )
 }
